e2a560f7f675ceaf21dcdfbb996962bed7b1556a
Harits <haritsirawan2118@gmail.com>
2024-06-11 10:14:47 +0700

    chore: Update primary color in Tailwind CSS configuration

---
+++ src/pages/dashboard.js
@@ -0,0 +1,12 @@
+import Login from "@/components/login";
+import Image from "next/image";
+import "@/styles/globals.css"
+import ImportData from "@/components/ImportData";
+
+export default function Home() {
+  return (
+    <>
+      <ImportData/>
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Login from "@/components/login";
+import Image from "next/image";
+import "@/styles/globals.css"
+import ImportData from "@/components/ImportData";
+
+export default function login() {
+  return (
+    <>
+      <Login/>
+      {/* <ImportData/> */}
+    </>
+  );
+}
