aaecc481d0c041ba350a9db39ccbff89e2057f40
Harits <haritsirawan2118@gmail.com>
2024-07-11 21:36:43 +0700

    feat: Enable LoginFix component and update Navbar styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,9 @@ import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className='flex mx-20 flex-col items-center px-5 pt-3.5 pb-20 font-medium bg-slate-100'>
       <Navbar />
-      <main className="container mx-auto p-4">
+      <main className="container mx-auto p-6">
         {children}
       </main>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,40 +10,42 @@ const navItems = [
 
 export default function Navbar() {
   return (
-    <nav className="flex w-full text-white rounded-3xl py-3 px-5 bg-gradient-to-r from-[#188D88] to-[#16B3AC] gap-5 justify-between items-center max-md:flex-wrap">
-      <a href="#home" className="flex items-center" aria-label="Home">
-        <img
-          loading="lazy"
-          src="/image/logo_gjls.svg"
-          alt="Logo"
-          className="shrink-0 self-stretch max-w-full aspect-[2.22] w-[180px]"
-        />
-      </a>
-      {navItems.map((item, index) => (
-        <div
-          key={index}
-          className={`self-stretch my-auto ${
-            item.hasHighlight ? "flex flex-col" : ""
-          }`}
+    <div className="flex gap-5 justify-between self-stretch py-3.5 pr-11 pl-5 w-full text-sm text-white rounded-3xl max-md:flex-wrap max-md:px-5 max-md:max-w-full">
+      <nav className="flex w-full text-white rounded-3xl py-3 px-5 bg-gradient-to-r from-[#188D88] to-[#16B3AC] gap-5 justify-between items-center max-md:flex-wrap">
+        <a href="#home" className="flex items-center" aria-label="Home">
+          <img
+            loading="lazy"
+            src="/image/logo_gjls.svg"
+            alt="Logo"
+            className="shrink-0 self-stretch max-w-full aspect-[2.22] w-[180px]"
+          />
+        </a>
+        {navItems.map((item, index) => (
+          <div
+            key={index}
+            className={`self-stretch my-auto ${
+              item.hasHighlight ? "flex flex-col" : ""
+            }`}
+          >
+            <a href={item.href}>{item.text}</a>
+            {item.hasHighlight && (
+              <div className="shrink-0 mt-1.5 bg-yellow-400 rounded-xl h-[3px]" />
+            )}
+          </div>
+        ))}
+        <a
+          href="#profile"
+          className="w-11 aspect-square"
+          aria-label="User Profile"
         >
-          <a href={item.href}>{item.text}</a>
-          {item.hasHighlight && (
-            <div className="shrink-0 mt-1.5 bg-yellow-400 rounded-xl h-[3px]" />
-          )}
-        </div>
-      ))}
-      <a
-        href="#profile"
-        className="w-11 aspect-square"
-        aria-label="User Profile"
-      >
-        <img
-          loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/d212aa7ed7246c8356b9cdb33f4f4cd184e73fcee1743e6da25f633cba3c5c01?apiKey=7879ad012efb4de194a58d39e8a3a4a5&"
-          alt="User Profile"
-          className="shrink-0 my-auto w-11 aspect-square"
-        />
-      </a>
-    </nav>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/d212aa7ed7246c8356b9cdb33f4f4cd184e73fcee1743e6da25f633cba3c5c01?apiKey=7879ad012efb4de194a58d39e8a3a4a5&"
+            alt="User Profile"
+            className="shrink-0 my-auto w-11 aspect-square"
+          />
+        </a>
+      </nav>
+    </div>
   );
 }
